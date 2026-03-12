c5d937dcf3139736263e4b84c6251697ea6cc87b
jsj <sungju3115@naver.com>
2026-03-13 08:59:29 +0900

    fix: @Transactional 추가

--- src/main/java/com/sprint/mission/findex/indexinfo/service/IndexInfoService.java
+++ src/main/java/com/sprint/mission/findex/indexinfo/service/IndexInfoService.java
@@ -2,6 +2,7 @@ package com.sprint.mission.findex.indexinfo.service;
 
 import com.sprint.mission.findex.exception.BusinessLogicException;
 import com.sprint.mission.findex.exception.ErrorCode;
+import com.sprint.mission.findex.indexinfo.SourceType;
 import com.sprint.mission.findex.indexinfo.dto.request.IndexInfoUpdateRequest;
 import com.sprint.mission.findex.indexinfo.mapper.IndexInfoMapper;
 import com.sprint.mission.findex.indexinfo.repository.IndexInfoRepository;
@@ -10,9 +11,11 @@ import com.sprint.mission.findex.indexinfo.dto.response.IndexInfoDto;
 import com.sprint.mission.findex.indexinfo.entity.IndexInfo;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
+@Transactional
 public class IndexInfoService {
     private final IndexInfoRepository indexInfoRepository;
     // private final IndexDataRepository indexDataRepository;
@@ -22,6 +25,7 @@ public class IndexInfoService {
     /*
     지수 정보 등록 (사용자 수동)
      */
+    @Transactional
     public IndexInfoDto createIndexInfo(IndexInfoCreateRequest request) {
         // 중복 검사
         boolean exists = indexInfoRepository.existsByIndexClassificationAndIndexName(
@@ -34,18 +38,24 @@ public class IndexInfoService {
         }
 
         IndexInfo indexInfo = indexInfoMapper.toEntity(request);
+        indexInfo.updateSourceType(SourceType.USER);
         IndexInfo savedIndexInfo = indexInfoRepository.save(indexInfo);
 
         // TODO:자동 연동 설정 등록 및 저장
+
         /*
         자동 연동 설정은 비활성화 상태로 등록
          */
+
+        // TODO: 자동 연동 설정 , 지수 정보 주입
+
         return indexInfoMapper.toDto(savedIndexInfo);
     }
 
     /*
     지수 정보 조회 (단건 조회)
      */
+    @Transactional(readOnly = true)
     public IndexInfoDto getIndexInfoById(Long id) {
         IndexInfo indexInfo = indexInfoRepository.findById(id)
                 .orElseThrow(() -> new BusinessLogicException(ErrorCode.INDEX_INFO_NOT_FOUND));
@@ -55,18 +65,22 @@ public class IndexInfoService {
     /*
     지수 정보 삭제
      */
+    @Transactional
     public void deleteIndexInfoById(Long id) {
         IndexInfo indexInfo = indexInfoRepository.findById(id)
                 .orElseThrow(() -> new BusinessLogicException(ErrorCode.INDEX_INFO_NOT_FOUND));
 
-        // TODO: IndexData 구현 후 삭제 되도록 추가 반영 필요 (Join vs Id로 삭제할 것인지?)
+        // TODO: IndexData 구현 후 삭제 되도록 추가 반영 필요
         //indexDataRepository.deleteByIndexInfoDataId(indexInfo.getId());
+
+        // TODO: SyncJob 삭제 로직 추가
         indexInfoRepository.deleteById(indexInfo.getId());
     }
 
     /*
     지수 정보 수정 (사용자 수동)
      */
+    @Transactional
     public IndexInfoDto updateIndexInfoById(Long id, IndexInfoUpdateRequest request) {
         IndexInfo indexInfo = indexInfoRepository.findById(id)
                 .orElseThrow(() -> new BusinessLogicException(ErrorCode.INDEX_INFO_NOT_FOUND));
