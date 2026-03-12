b147f33687aaf64c3840fd3736a95d06cd3f031e
jsj <sungju3115@naver.com>
2026-03-12 17:21:06 +0900

    feat: IndexInfo Entity 구현

--- src/main/java/com/sprint/mission/findex/indexinfo/IndexInfo.java
+++ src/main/java/com/sprint/mission/findex/indexinfo/IndexInfo.java
@@ -15,28 +15,36 @@ import java.time.LocalDate;
 @AllArgsConstructor
 @Builder
 @Entity
-@Table(name = "index_info")
+@Table(
+        name = "index_info",
+        uniqueConstraints = {
+                @UniqueConstraint(
+                        name = "uk_index_classification_index_name",
+                        columnNames = {"index_classification", "index_name"}
+                )
+        }
+)
 public class IndexInfo extends BaseEntity {
-
+    // 지수 분류명
     @Column(name = "index_classification", length = 100, nullable = false)
     private String indexClassification;
-
+    // 지수 이름
     @Column(name = "index_name", length = 100, nullable = false)
     private String indexName;
-
+    // 채용 종목 수
     @Column(name = "employed_items_count")
     private Integer employedItemsCount;
-
+    // 기준 시점
     @Column(name = "base_point_in_time")
     private LocalDate basePointInTime;
-
+    // 기준 지수
     @Column(name = "base_index", precision = 15, scale = 2)
     private BigDecimal baseIndex;
 
     @Enumerated(EnumType.STRING)
     @Column(name = "source_type", nullable = false)
     private SourceType sourceType;
-
+    // 즐겨찾기
     @Column(name = "favorite", nullable = false)
     private boolean favorite;
 }
