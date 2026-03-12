2864608369c3c0dd9f20f445820712422b6d13d0
rorm0819 <rorm0819@gmail.com>
2026-03-12 15:30:22 +0900

    Findex 프로젝트 틀

--- src/main/java/com/sprint/mission/findex/BaseEntitiy.java
+++ src/main/java/com/sprint/mission/findex/BaseEntitiy.java
@@ -1,8 +1,11 @@
 package com.sprint.mission.findex;
 
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.MappedSuperclass;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.CreatedDate;
 
 import java.time.Instant;
@@ -10,15 +13,13 @@ import java.util.UUID;
 
 @Getter
 @MappedSuperclass
+@NoArgsConstructor
 public class BaseEntitiy {
     @Id
-    private UUID id;
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
     @CreatedDate
     private Instant createdAt;
     @CreatedDate
     private Instant updatedAt;
-
-    public BaseEntitiy() {
-        this.id = UUID.randomUUID();
-    }
 }
