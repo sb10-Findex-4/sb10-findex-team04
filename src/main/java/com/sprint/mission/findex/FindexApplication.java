75ace252abafa1f0b6f791a589dcb22e3baf90e7
rorm0819 <rorm0819@gmail.com>
2026-03-12 11:39:15 +0900

    빈 프로젝트

---
+++ src/main/java/com/sprint/mission/findex/FindexApplication.java
@@ -0,0 +1,13 @@
+package com.sprint.mission.findex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FindexApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FindexApplication.class, args);
+    }
+
+}
