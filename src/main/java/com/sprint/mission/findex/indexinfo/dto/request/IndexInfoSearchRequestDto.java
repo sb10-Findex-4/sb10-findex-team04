dabc13fede3b84ae7296925487eb4f8fc1c781bc
jsj <sungju3115@naver.com>
2026-03-13 13:32:43 +0900

    feat: 지수 목록 조회 Dto 구현

--- src/main/java/com/sprint/mission/findex/indexinfo/dto/request/IndexInfoSearchRequestDto.java
+++ src/main/java/com/sprint/mission/findex/indexinfo/dto/request/IndexInfoSearchRequestDto.java
@@ -1,4 +1,15 @@
 package com.sprint.mission.findex.indexinfo.dto.request;
-// TODO
-public record IndexInfoSearchRequestDto() {
+/*
+지수 정보 목록 조회 Dto
+ */
+public record IndexInfoSearchRequestDto(
+        String indexClassification,
+        String indexName,
+        boolean favorite,
+        Long isAfter,
+        String cursor,
+        String sortField,
+        String sortDirection,
+        int size
+) {
 }
