32175a2ee4a8d22622f6f11d06e85b7b027aa6ad
jsj <sungju3115@naver.com>
2026-03-13 16:42:47 +0900

    fix: 틀린 주석 삭제

--- src/main/java/com/sprint/mission/findex/indexinfo/dto/request/IndexInfoSearchRequestDto.java
+++ src/main/java/com/sprint/mission/findex/indexinfo/dto/request/IndexInfoSearchRequestDto.java
@@ -3,13 +3,18 @@ package com.sprint.mission.findex.indexinfo.dto.request;
 지수 정보 목록 조회 Dto
  */
 public record IndexInfoSearchRequestDto(
+        // 지수 분류명
         String indexClassification,
+        // 지수명
         String indexName,
-        boolean favorite,
+        // 즐겨찾기
+        Boolean favorite,
+        // 커서 (id)
         Long isAfter,
+
         String cursor,
         String sortField,
         String sortDirection,
-        int size
+        Integer size
 ) {
 }
