bdfa20def3045739d978c2c8a644f568b2074328
LeeDyol <dlekthf819203@naver.com>
2026-03-13 14:29:22 +0900

    feat: @ModelAttribute 적용 및 프론트엔드 규격 맞춤 컨트롤러 수선

--- src/main/java/com/sprint/mission/findex/syncJob/controller/SyncJobController.java
+++ src/main/java/com/sprint/mission/findex/syncJob/controller/SyncJobController.java
@@ -1,5 +1,6 @@
 package com.sprint.mission.findex.syncJob.controller;
 
+import com.sprint.mission.findex.syncJob.dto.request.SyncJobSearchConditionDto;
 import com.sprint.mission.findex.syncJob.dto.response.CursorPageResponseSyncJobDto;
 import com.sprint.mission.findex.syncJob.entity.SyncJob;
 import com.sprint.mission.findex.syncJob.service.SyncService;
@@ -7,10 +8,8 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
 
 @Tag(name = "SyncJob", description = "SyncJob API")
 @RestController
@@ -19,25 +18,22 @@ import org.springframework.web.bind.annotation.RestController;
 public class SyncJobController {
     private final SyncService syncService;
 
-    // 연동 작업 목록 조회
+    /*
+        연동 작업 목록 조회
+        ---------------
+        Swagger APU 명세서에 정의된 파라미터 개수가 많아 하나의 Dto로 묶어서 처리
+        별도로 default 값이 명시된 필드만 컨트롤러의 파라미터로 주입
+    */
     @Operation(summary = "연동 작업 목록 조회", operationId = "getSyncJobs")
     @GetMapping
     public ResponseEntity<CursorPageResponseSyncJobDto<SyncJob>> findAllSyncJobs(
-            @RequestParam(required = false) String jobType,
-            @RequestParam(required = false) Long indexInfoId,
-            @RequestParam(required = false) String baseDateFrom,
-            @RequestParam(required = false) String baseDateTo,
-            @RequestParam(required = false) String worker,
-            @RequestParam(required = false) String jobTimeFrom,
-            @RequestParam(required = false) String jobTimeTo,
-            @RequestParam(required = false) String status,
-            @RequestParam(required = false) Long idAfter,
-            @RequestParam(required = false) String cursor,
-            @RequestParam(required = false) String sortField,
-            @RequestParam(required = false) String sortDirection,
-            @RequestParam(defaultValue = "10") int size
-    ) {
-        CursorPageResponseSyncJobDto<SyncJob> response = syncService.findAllSyncJobs();
+            @ModelAttribute SyncJobSearchConditionDto syncJobSearchConditionDto,    // 연동 목록 조회 Request Dto
+            @RequestParam(defaultValue = "jobTime") String sortField,               // 정렬 필드
+            @RequestParam(defaultValue = "desc") String sortDirection,              // 정렬 방향 (ASC / DESC)
+            @RequestParam(defaultValue = "10") int size                             // 페이지 크기
+            ) {
+
+        CursorPageResponseSyncJobDto<SyncJob> response = syncService.findAllSyncJobs(syncJobSearchConditionDto, sortField, sortDirection, size);
 
         return ResponseEntity.ok(response);
     }
