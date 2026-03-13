2e754b11402b542f541b5e875f762fa58d401953
jsj <sungju3115@naver.com>
2026-03-13 16:39:42 +0900

    fix: basePointInTime 현재 날짜보다 아래만 생성하도록

--- src/main/java/com/sprint/mission/findex/indexinfo/dto/request/IndexInfoUpdateRequestDto.java
+++ src/main/java/com/sprint/mission/findex/indexinfo/dto/request/IndexInfoUpdateRequestDto.java
@@ -1,6 +1,7 @@
 package com.sprint.mission.findex.indexinfo.dto.request;
 
 import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.PastOrPresent;
 import jakarta.validation.constraints.PositiveOrZero;
 
 import java.math.BigDecimal;
@@ -12,6 +13,7 @@ public record IndexInfoUpdateRequestDto(
         @PositiveOrZero
         Integer employedItemsCount,
 
+        @PastOrPresent
         LocalDate basePointInTime,
 
         @DecimalMin(value = "0.0", inclusive = true)
