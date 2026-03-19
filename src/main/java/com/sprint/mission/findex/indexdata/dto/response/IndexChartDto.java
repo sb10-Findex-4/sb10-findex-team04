d40492c383585222d689c001b6f74bdc5c23beec
jsj <sungju3115@naver.com>
2026-03-19 16:26:05 +0900

    fix: 20일 이동 평균 미출력 문제 해결

--- src/main/java/com/sprint/mission/findex/indexdata/dto/response/IndexChartDto.java
+++ src/main/java/com/sprint/mission/findex/indexdata/dto/response/IndexChartDto.java
@@ -1,13 +1,15 @@
 package com.sprint.mission.findex.indexdata.dto.response;
 
+import com.sprint.mission.findex.indexdata.entity.PeriodType;
+
 import java.util.List;
 
 public record IndexChartDto(
-    Long indexInfoId,                     // 지수 정보 ID
-    String indexClassification,           // 지수 구분
-    String indexName,                     // 지수 이름
-    String periodType,                    // 조회 기간
-    List<Object> dataPoints,      // 실제 지수 데이터
-    List<Object> ma5DataPoints,   // 5일 이동평균선 데이터
-    List<Object> ma20DataPoints   // 20일 이동평균선 데이터
+        Long indexInfoId,                     // 지수 정보 ID
+        String indexClassification,           // 지수 구분
+        String indexName,                     // 지수 이름
+        PeriodType periodType,                    // 조회 기간
+        List<Object> dataPoints,      // 실제 지수 데이터
+        List<Object> ma5DataPoints,   // 5일 이동평균선 데이터
+        List<Object> ma20DataPoints   // 20일 이동평균선 데이터
 ) {}
